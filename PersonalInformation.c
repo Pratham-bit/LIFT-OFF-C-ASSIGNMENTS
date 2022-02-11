9ba5c199e86763da65619fb54f2365f706f11247
Pratham-bit <85451959+Pratham-bit@users.noreply.github.com>
2022-02-11 19:43:27 +0530

    Add files via upload

---
+++ PersonalInformation.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    char a[50];int e;char b[50];char c[100];
+    printf("Name of the student:\n");
+    gets(a);
+    printf("Regd No.:\n");
+    gets(e);
+    printf("Branch:\n");
+    gets(b);
+    printf("My Hobbies are\n");
+    gets(c);
+    return 0;
+}
