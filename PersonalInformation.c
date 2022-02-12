2097ff7d8b2ebd79d15d3f805ca3e35adae74774
Pratham-bit <85451959+Pratham-bit@users.noreply.github.com>
2022-02-12 18:11:38 +0530

    Update PersonalInformation.c

--- PersonalInformation.c
+++ PersonalInformation.c
@@ -4,11 +4,12 @@ int main()
     char a[50];int e;char b[50];char c[100];
     printf("Name of the student:\n");
     gets(a);
-    printf("Regd No.:\n");
-    gets(e);
     printf("Branch:\n");
     gets(b);
     printf("My Hobbies are\n");
     gets(c);
+    printf("Regd No.:\n");
+    scanf("%d",&e);
+    printf("Name:%s\n Branch:%s\n Hobbies:%s\n", &a, &b, &c);
     return 0;
 }
